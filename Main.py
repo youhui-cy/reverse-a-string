0eeec13dcf78638229c28fd4340ba3633424afc2
youhui-cy <young.cy.hui@gmail.com>
2025-05-30 18:07:03 -0700

    1st commit

---
+++ Main.py
@@ -0,0 +1,25 @@
+def slicing(text):
+    reversed_text = text[::-1]
+    print(reversed_text)  # Output reverse string via slicing method.
+
+def reverse_and_join(text):
+    reversed_text = ''.join(reversed(text))
+    print(reversed_text)  # Output reverse string via reverse() and join().
+
+def loop(text):
+    reversed_text = ""
+    for char in text:
+        reversed_text = char + reversed_text
+    print(reversed_text)  # Output reverse string via for loop method.
+
+
+text = input("Enter a string: ")
+print("You entered:", text)
+method = input("Enter a method: slicing, join, or loop:")
+if method == "slicing":
+    slicing(text)
+if method == "join":
+    reverse_and_join(text)
+if method == "loop":
+    loop(text)
+
